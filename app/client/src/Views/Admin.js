2638384d82bb5e1a84189a884ea76e741b521988
Dallas Baker <dallas.baker@sheldyn.io>
2021-03-27 06:44:25 -0700

    Built routes

--- app/client/src/Views/Admin.js
+++ app/client/src/Views/Admin.js
@@ -5,7 +5,8 @@ import Sidebar from '../Components/Navigation/Sidebar';
 import axios from 'axios';
 import Stats from '../Components/Stats';
 import { Button, Container, Row, Col, Table, Alert } from 'reactstrap';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Route } from 'react-router-dom';
+import routes from '../routes';
 
 const Admin = () => {
   const { authState } = useOktaAuth();
@@ -17,6 +18,19 @@ const Admin = () => {
     { Header: 'Name', accessor: 'Player' },
     { Header: 'Score', accessor: 'Score' },
   ]);
+
+  const getRoutes = (routes) => {
+    return routes.map((prop, key) => {
+      if (prop.collapse) {
+        return getRoutes(prop.views);
+      }
+      if (prop.layout === '/admin') {
+        return <Route path={prop.layout + prop.path} component={prop.component} key={key} />;
+      } else {
+        return null;
+      }
+    });
+  };
 
   const handleFileProcess = (processAlert) => {
     setProcessAlert(processAlert);
@@ -77,50 +91,30 @@ const Admin = () => {
       <Sidebar />
       <div className="content">
         <div className="wrapper">
-          <div className="space-110"></div>
-          <div className="space-110"></div>
-          <section className="section">
-            <Alert color="success" isOpen={processAlert} toggle={onDismiss}>
-              File was processed successfully.
-            </Alert>
-          </section>
-          <div className="space-110"></div>
-          <section className="section">
-            <Container>
-              <Row>
-                <Col col-md-7></Col>
-                <Col col-md-4>
-                  <h1 className="profile-title text-left text-left">Total Possible Points</h1>
-                  <h5 className="text-on-back">0</h5>
-                </Col>
-              </Row>
-              <Row>
-                <Uploader
-                  accessToken={authState.accessToken.accessToken}
-                  onProcessFile={handleFileProcess}
-                />
-              </Row>
-              <br />
-              <h2> BOB </h2>
-              <Row>
-                <Col>
-                  <div className="btn-wrapper">
-                    <Button onClick={handleRefresh} className="btn-simple" color="success">
-                      <i className="tim-icons icon-refresh-02" /> Refresh
-                    </Button>
-                    <Button onClick={handleReset} className="btn-simple" color="warning">
-                      <i className="tim-icons icon-refresh-01" /> Reset
-                    </Button>
-                    <Button onClick={handleReset} className="btn-simple" color="danger">
-                      <i className="tim-icons icon-trash-simple" /> Delete
-                    </Button>
-                  </div>
-                </Col>
-              </Row>
-
-              <Stats columns={col} data={stats} />
-            </Container>
-          </section>
+          <Row>
+            <Uploader
+              accessToken={authState.accessToken.accessToken}
+              onProcessFile={handleFileProcess}
+            />
+          </Row>
+          <br />
+          <h2> BOB </h2>
+          <Row>
+            <Col>
+              <div className="btn-wrapper">
+                <Button onClick={handleRefresh} className="btn-simple" color="success">
+                  <i className="tim-icons icon-refresh-02" /> Refresh
+                </Button>
+                <Button onClick={handleReset} className="btn-simple" color="warning">
+                  <i className="tim-icons icon-refresh-01" /> Reset
+                </Button>
+                <Button onClick={handleReset} className="btn-simple" color="danger">
+                  <i className="tim-icons icon-trash-simple" /> Delete
+                </Button>
+              </div>
+            </Col>
+          </Row>
+          <Stats columns={col} data={stats} />
         </div>
       </div>
     </div>
--- app/client/src/routes.js
+++ app/client/src/routes.js
@@ -1,2 +1,70 @@
 // This is used for admin dashboards only.
 // For main routing please see app.js
+
+const routes = [
+  {
+    path: '/users',
+    name: 'Users',
+    icon: 'tim-icons icon-chart-pie-36',
+    component: Users,
+    layout: '/admin',
+  },
+  {
+    path: '/cashGames',
+    name: 'Cash Games',
+    icon: 'tim-icons icon-chart-pie-36',
+    component: CashGame,
+    layout: '/admin',
+  },
+  {
+    path: '/tournaments',
+    name: 'Tournaments',
+    icon: 'tim-icons icon-chart-pie-36',
+    component: Tournaments,
+    layout: '/admin',
+  },
+  {
+    path: '/bob',
+    name: 'Bob',
+    icon: 'tim-icons icon-chart-pie-36',
+    component: Bob,
+    layout: '/admin',
+  },
+  {
+    path: '/cashHand',
+    name: 'Cash Hand',
+    icon: 'tim-icons icon-chart-pie-36',
+    component: CashHand,
+    layout: '/admin',
+  },
+  {
+    path: '/stats',
+    name: 'Stats',
+    icon: 'tim-icons icon-chart-pie-36',
+    component: Stats,
+    layout: '/admin',
+  },
+  {
+    path: '/seasons',
+    name: 'Seasons',
+    icon: 'tim-icons icon-chart-pie-36',
+    component: Seasons,
+    layout: '/admin',
+  },
+  {
+    path: '/messages',
+    name: 'Messages',
+    icon: 'tim-icons icon-chart-pie-36',
+    component: Messages,
+    layout: '/admin',
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    icon: 'tim-icons icon-chart-pie-36',
+    component: Settings,
+    layout: '/admin',
+  },
+];
+
+export default routes;
