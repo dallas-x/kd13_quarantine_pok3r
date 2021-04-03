42e2f4c55ee3f7d4420c057031afaa4fa19cc40c
Dallas Baker <dallas.baker@sheldyn.io>
2021-04-03 12:10:19 -0700

    fix(users): signin redirect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom';
 import { Security, LoginCallback } from '@okta/okta-react';
 import { OktaAuth } from '@okta/okta-auth-js';
-import config from './app.config.js';
 import Navi from './Components/Navi';
 import Home from './Views/Home';
 import Admin from './Views/Admin';
@@ -13,7 +12,18 @@ import Registration from './Views/Registration';
 import Login from './Views/Login';
 
 const App = () => {
-  const oktaAuth = new OktaAuth(config.oidc);
+  const history = useHistory();
+  const onAuthRequired = () => {
+    history.push('/login');
+  };
+
+  const oktaAuth = new OktaAuth({
+    issuer: `https://${process.env.OKTA_DOMAIN}.okta.com/oauth2/default`,
+    clientId: process.env.OKTA_REACT_CLIENTID,
+    redirectUri: window.location.origin + process.env.REDIRECT_URI,
+    onAuthRequired: onAuthRequired,
+    pkce: true,
+  });
   return (
     <Router>
       <Security oktaAuth={oktaAuth}>
--- src/app.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  oidc: {
-    issuer: `https://${process.env.OKTA_DOMAIN}.okta.com/oauth2/default`,
-    clientId: process.env.OKTA_REACT_CLIENTID,
-    redirectUri: process.env.REDIRECT_URI,
-  },
-};
